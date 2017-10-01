0ff7a63f84492459139e716649c2b064a5fc947a
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-02 01:52:55 +0300

    Третье задание

---
+++ task_01_03.py
@@ -0,0 +1,22 @@
+print('Введите три целых числа (например: 10 4 6')
+
+l = input().split()
+
+for i in range(len(l)):
+    l[i] = int(l[i])
+
+n = 1
+
+while n < len(l):
+    for i in range(len(l)-n):
+        if l[i] >l[i+1]:
+            l[i],l[i+1] = l[i+1],l[i]
+    n += 1
+
+for i in range(len(l)):
+    l[i] = str(l[i])
+    
+
+print(', '.join(l))
+
+
