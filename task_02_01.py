dd9e106d8ad034bf2c1d116ec7216bfab5203186
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-09 17:09:21 +0300

    Исправления2

--- task_02_01.py
+++ task_02_01.py
@@ -1,6 +1,5 @@
 def is_palindrome(s):
-    
-    def vfun(sh):              
+    def vfun(sh): 
         sh = str(sh)
         sh = sh.lower()
         L = list(' !@#%^&*();:?,.\/|')
@@ -9,9 +8,5 @@ def is_palindrome(s):
         spi = list(sh)
         spi.reverse()
         sk = ''.join(spi)
-        if sh != sk:
-            print(False)
-        else:
-            print(True)
-        return sh
+        return False if sh != sk else True
     return vfun
