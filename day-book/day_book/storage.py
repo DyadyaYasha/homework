fe8b80034d2eff73ecdb2d6785573867be42ef37
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-11 17:53:29 +0300

    отпочковал меню

--- day-book/day_book/storage.py
+++ day-book/day_book/storage.py
@@ -1,3 +1,4 @@
+import menu
 import os.path as Path
 import sqlite3
 
@@ -6,85 +7,45 @@ SQL_SELECT_ALL = """
     FROM daybook
 """ 
 
-def menu():
-    loop = True
-    while loop == True:
-        print(
-            '\n Ежедневник. Выберите действие:\n\n'
-            ' 1 - Вывести список задач\n'
-            ' 2 - Добавить задачу\n'
-            ' 3 - Отредактировать задачу\n'
-            ' 4 - Завершить задачу\n'
-            ' 5 - Начать задачу сначала\n'
-            ' 0 - Выход'
-        )
-        vvod = input('\n Введите число: ')
+menu.menu()
 
-        if vvod == '1':
-            spizad()
-        elif vvod == '2':
-            print('\n Ввввжух и добавляю задачу\n')
-        elif vvod == '3':
-            print('\n Ввввжух и редактирую задачу\n')
-        elif vvod == '4':
-            print('\n Ввввжух и завершаю задачу\n')
-        elif vvod == '5':
-            print('\n Ввввжух и начинаю задачу сначала\n')
-        elif vvod == '0':
-            print('\n Ввввжух и выхожу\n')
-            loop = False
-        else:
-            print('\n Неизвестная команда. Попробуй снова. Я знаю ты сможешь!\n')
+def connect(db_name=None):
+    if db_name is None:
+        db_name = 'daybook.sqlite'
 
-def spizad():
-    loop2 = True
-    while loop2 == True:
-        print(
-            '\n Список задач. Выберите действие:\n\n'
-            ' 1 - За день\n'
-            ' 2 - За неделю\n'
-            ' 3 - За месяц\n'
-            ' 0 - Вернуться в главное меню'
-        )
-        vvod2 = input('\n Введите число: ')
+    conn = sqlite3.connect(db_name)
+    # магия
 
-        if vvod2 == '1':
-            #print('\n Ввввжух и вывожу список задач за день\n')
-            zaden()
-        elif vvod2 == '2':
-            print('\n Ввввжух и вывожу список задач за неделю\n')
-        elif vvod2 == '3':
-                print('\n Ввввжух и вывожу список задач за месяц\n')
-        elif vvod2 == '0':
-            print('\n Ввввжух и возвращаюсь в главное меню\n')
-            loop2 = False
-        else:
-            print('\n Неизвестная команда. Попробуй снова. Я знаю ты сможешь!\n')
+    return conn
 
-def zaden():
-    loop3 = True
-    while loop3 == True:
-        print(
-            '\n Список задач. За день. Выберите действие:\n\n'
-            ' 1 - За сегодня\n'
-            ' 2 - За конкретный день\n'
-            ' 0 - Вернуться назад'
-        )
-        vvod3 = input('\n Введите число: ')
-
-        if vvod3 == '1':
-            print('\n Ввввжух и вывожу список задач за сегодня\n')
-        elif vvod3 == '2':
-            print('\n Ввввжух и вывожу список задач за конкретный день\n')
-        elif vvod3 == '0':
-            print('\n Ввввжух и возвращаюсь назад\n')
-            loop3 = False
-        else:
-            print('\n Неизвестная команда. Попробуй снова. Я знаю ты сможешь!\n')
+def initialize(conn, creation_schema):
+    with conn, open(creation_schema) as f:
+        conn.executescript(f.read())
 
 
-if (__name__=="__main__"):
-    menu()
+def add_zad(conn, zad):
+    """Сохраняет новую задачу в базу"""
+
+
+def zad_all(conn):
+    """Возвращает все задачи из базы"""
+
+
+def zad_by_pk(conn, pk):
+    """Возвращает задачу по первичному ключу"""   
+
+
+def zad_by_zagol(conn, zagol):
+    """Возвращает задачу по короткому заголовоку"""
+
+
+def zad_by_dm(conn, day):
+    """Возвращает задачи по конкретному деню""" 
+
+def zad_by_dm(conn, day, month):
+    """Возвращает задачи по дате день + месяц""" 
+
+
 
 
 
