3208fa925045ce426278218886168157b303a60b
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-13 20:57:45 +0300

    Исправления2

--- task_02_01.py
+++ task_02_01.py
@@ -1,16 +1,3 @@
-# def is_palindrome(s):
-#     def vfun(sh): 
-#         sh = str(sh)
-#         sh = sh.lower()
-#         L = list(' !@#%^&*();:?,.\/|')
-#         for i in L:
-#             sh = sh.replace(i, '')
-#         spi = list(sh)
-#         spi.reverse()
-#         sk = ''.join(spi)
-#         return False if sh != sk else True
-#     return vfun
-
 def is_palindrome(s): 
     sh = str(s)
     sh = sh.lower()
