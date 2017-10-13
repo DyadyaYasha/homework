5f90c62f9966dac4b381db3ad88c5a777de9dd2e
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-13 20:56:06 +0300

    Исправления2

--- task_02_01.py
+++ task_02_01.py
@@ -1,12 +1,26 @@
-def is_palindrome(s):
-    def vfun(sh): 
-        sh = str(sh)
-        sh = sh.lower()
-        L = list(' !@#%^&*();:?,.\/|')
-        for i in L:
-            sh = sh.replace(i, '')
-        spi = list(sh)
-        spi.reverse()
-        sk = ''.join(spi)
-        return False if sh != sk else True
-    return vfun
+# def is_palindrome(s):
+#     def vfun(sh): 
+#         sh = str(sh)
+#         sh = sh.lower()
+#         L = list(' !@#%^&*();:?,.\/|')
+#         for i in L:
+#             sh = sh.replace(i, '')
+#         spi = list(sh)
+#         spi.reverse()
+#         sk = ''.join(spi)
+#         return False if sh != sk else True
+#     return vfun
+
+def is_palindrome(s): 
+    sh = str(s)
+    sh = sh.lower()
+    L = list(' !@#%^&*();:?,.\/|')
+    for i in L:
+        sh = sh.replace(i, '')
+    spi = list(sh)
+    spi.reverse()
+    sk = ''.join(spi)
+    return False if sh != sk else True
+
+if (__name__=="__main__"):
+    print(is_palindrome('Spam Aps'))
