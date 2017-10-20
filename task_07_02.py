c9b3f8ffd3ae6b2d5647e421611a125a8eb079ff
Dyadya_yasha <dyadyayasha@gmail.com>
2017-10-20 15:01:56 +0300

    Случайный пароль

--- task_07_02.py
+++ task_07_02.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def passgen(xxx):
+def password_generator(xxx):
     znaki = (string.ascii_letters + string.digits + string.punctuation)
     #sym = (string.ascii_letters + string.digits)
     cikla = (random.choice(znaki) for i in range(xxx))
