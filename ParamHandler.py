956d53c9c4e173935f7a2ed195a567657aa2f94a
Dyadya_yasha <dyadyayasha@gmail.com>
2017-11-03 00:12:23 +0300

    ParamHandler дополнение кода из лекции(finish)

--- ParamHandler.py
+++ ParamHandler.py
@@ -1,5 +1,10 @@
 from abc import ABCMeta, abstractmethod
 from os import path
+import pickle
+import json
+
+class ParamHandlerException(Exception):
+    pass
 
 class ParamHandler(metaclass=ABCMeta):
     def __init__(self, source):
@@ -54,10 +59,51 @@ class TxtParamHandler(ParamHandler):
     #     with open(self.source, 'a') as f:
     #         f.write(data2)
 
+
+class JsonParamHandler(ParamHandler):
+
+    def read(self):
+        with open(self.source, 'r') as f:
+            return json.load(f)
+
+    def write(self, data):
+        with open(self.source, 'w') as f:
+            json.dump(data, f)
+
+
+class PickleParamHandler(ParamHandler):
+
+    def read(self):
+        with open(self.source, 'rb') as f:
+            return pickle.load(f)
+
+    def write(self, data):
+        with open(self.source, 'wb') as f:
+            pickle.dump(data, f)
+
 if __name__ == '__main__':
 
     ParamHandler.add_type('txt', TxtParamHandler)
     r = ParamHandler.get_instance('./data.txt')
     r.write('Это запишется в файл')
-
     print(r.read())
+
+    ParamHandler.add_type('json', JsonParamHandler)
+    j = ParamHandler.get_instance('./data.json')
+    params = {
+        'key1': 'val1',
+        'key2': 'val2',
+        'key3': 'val3'
+    }
+    j.write(params)
+    print(j.read())
+
+    ParamHandler.add_type('pickle', PickleParamHandler)
+    p = ParamHandler.get_instance('./data.pickle')
+    params = {
+        'key1': 'val1',
+        'key2': 'val2',
+        'key3': 'val3'
+    }
+    p.write(params)
+    print(p.read())
